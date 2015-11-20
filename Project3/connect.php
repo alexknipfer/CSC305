54615ece4552fae24593971b61d13aa00e7fecff
alexknipfer <alexanderknipfer@gmail.com>
2015-11-20 20:28:49 +0000

    now creates tables upon a valid sql connection

--- Project3/connect.php
+++ Project3/connect.php
@@ -1,17 +1,34 @@
 <?php
 
     $servername = $_POST['servername'];
+    $databaseName = $_POST['dbname'];
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = mysqli_connect($servername, $username, $password, $databaseName);
 
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-   }
+    if (!$conn) {
+        header("Location: error.html");
+    }
     
-    echo "Connected successfully";
-
+    else{
+        $recipeTable = "create table if not exists Recipes(RecipeName varchar(100), ";
+        $recipeTable .= "Ingredient varchar(100), Quantity int, primary key(recipeName, ingredient))";
+        
+        $result = mysqli_query($conn, $recipeTable);
+        
+        $inventoryTable = "create table if not exists Inventory (Ingredient varchar(100), ";
+        $inventoryTable .= "Quantity int)";
+        
+        $result = mysqli_query($conn, $inventoryTable);
+        
+        
+        header("Location: main.html");
+    }
+    
+    //echo "Connected successfully";
+    
 ?>
